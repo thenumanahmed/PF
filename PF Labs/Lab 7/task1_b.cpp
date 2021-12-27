0e65696a2817bfa26189e31a9be86f2476ad552d
Numan Ahmed <numanahmedmail@gmail.com>
2021-12-27 11:26:26 +0500

    lab 7 tasks

---
+++ PF Labs/Lab 7/task1_b.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "This program wil show you the greatest of 2 numbers. " << endl;
+    int num1 = 0, num2 = 0;
+    cout << endl << "Enter first number: ";
+    cin >> num1;
+    cout << endl << "Enter second number: ";
+    cin >> num2;
+    switch (num1 > num2)
+    {
+    case 0:
+        switch (num1 == num2) {
+        case 1:
+            cout << endl << "Both numbers are equal. " << endl;
+            break;
+        case 0:
+            cout << endl << "The number " << num1 << " is smaller " << endl;
+            cout << endl << "The number " << num2 << " is greater " << endl;
+            break;
+        }
+        break;
+    case 1:
+        cout << endl << "The number " << num2 << " is smaller " << endl;
+        cout << endl << "The number " << num1 << " is greater " << endl;
+        break;
+    }
+    return 0;
+}
